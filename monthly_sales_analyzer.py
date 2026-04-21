7fbb06511fe42d1911c612830e2e93b619f3c3ce
Maria Veronica Suarez Vidal <veronicasuarezvidal@gmail.com>
2026-04-21 17:32:00 +0000

    Completed exercises

--- monthly_sales_analyzer.py
+++ monthly_sales_analyzer.py
@@ -24,27 +24,43 @@ sales_data = [
 
 def total_sales_by_product(data, product_key):
     """Calculates the total sales of a specific product in 30 days."""
-    pass
+    total = 0
+    for day_data in data: 
+        total += day_data[product_key]
+    return total
 
 
 def average_daily_sales(data, product_key):
     """Calculates the average daily sales of a specific product."""
-    pass
+    total = total_sales_by_product (data,product_key)
+    return total / len(data)
 
 
 def best_selling_day(data):
     """Finds the day with the highest total sales."""
-    pass
-
+    best_day = data [0]["day"]
+    max_sales = 0
+    for day_data in data:
+        daily_total = day_data ["product_a"] + day_data ["product_b"] + day_data ["product_c"]
+        if daily_total > max_sales:
+            max_sales = daily_total
+            best_day = day_data ["day"]
+    return best_day
 
 def days_above_threshold(data, product_key, threshold):
     """Counts how many days the sales of a product exceeded a given threshold."""
-    pass
+    count = 0
+    for day_data in data:
+        if day_data[product_key]>threshold:
+           count += 1
+    return count
 
 
 def top_product(data):
     """Determines which product had the highest total sales in 30 days."""
-    pass
+    products = ["product_a","product_b","product_c"]
+    totals = {p:total_sales_by_product(data, p) for p in products}
+    return max(totals, key=totals.get)
 
 
 
